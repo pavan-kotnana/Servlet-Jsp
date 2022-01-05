81f81e765b692f2978e1173af8704c185361dcde
pavan-kotnana <87119981+pavan-kotnana@users.noreply.github.com>
2022-01-05 16:57:07 +0530

    using session

--- sqServlet.java
+++ sqServlet.java
@@ -9,10 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 public class sqServlet extends HttpServlet
 {	
 	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
-		
-	     	int k = Integer.parseInt(req.getParameter("k"));
-		k=k*k;
-		res.getWriter().println("result is :"+k);
+	
+		HttpSession session = req.getSession();
+		int k = (int)session.getAttribute("k");
+		k = k*k;
+		PrintWriter out = res.getWriter();
+		out.println("result is :" +k);
 		
 	}
 	
