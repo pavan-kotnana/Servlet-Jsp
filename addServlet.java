422ac281b7071edcb56e9e1967bbaa651fe78e39
pavan-kotnana <87119981+pavan-kotnana@users.noreply.github.com>
2022-01-05 16:58:48 +0530

    using sessions

--- addServlet.java
+++ addServlet.java
@@ -15,8 +15,10 @@ public class addServlet extends HttpServlet {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i+j;
-		res.sendRedirect("sq?k="+k); // what we have done here is URL rewriting comes under session management other are session & cookies
-	 
+		HttpSession session = req.getSession();
+		session.setAttribute("k", k);
+		res.sendRedirect("sq");
+	
 	}
 
 }
