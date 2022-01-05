5e1052dd1f0ffb8a2028f634f943f63444dde51b
pavan-kotnana <87119981+pavan-kotnana@users.noreply.github.com>
2022-01-05 17:51:02 +0530

    using Cookie

--- addServlet.java
+++ addServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
 
 @SuppressWarnings("serial")
 public class addServlet extends HttpServlet {
@@ -15,9 +16,9 @@ public class addServlet extends HttpServlet {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i+j;
-		HttpSession session = req.getSession();
-		session.setAttribute("k", k);
-		res.sendRedirect("sq");
+			Cookie cookie = new Cookie("k",k+"");
+			res.addCookie(cookie);
+			res.sendRedirect("sq");
 	
 	}
 
