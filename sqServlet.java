43ca4d93d3e4e9836ee30032b376767c1ddd7d7b
pavan-kotnana <87119981+pavan-kotnana@users.noreply.github.com>
2022-01-06 15:16:00 +0530

    replacing xml with annotation

--- sqServlet.java
+++ sqServlet.java
@@ -1,11 +1,12 @@
  package com.pavan;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import javax.servlet.annotation.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
+@WebServlet("/sq")
 public class sqServlet extends HttpServlet
 {	
 	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
