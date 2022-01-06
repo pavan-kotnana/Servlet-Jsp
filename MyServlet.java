f43d018d1acdb4f2fb4d07a4dc86160bebc9acec
pavan-kotnana <87119981+pavan-kotnana@users.noreply.github.com>
2022-01-06 16:34:29 +0530

    context-config

---
+++ MyServlet.java
@@ -0,0 +1,25 @@
+package com.pavan;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException ,IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.println("hi ");
+	ServletContext ctx = getServletContext();
+	String str = ctx.getInitParameter("name");  //this method gives the value of the specified attribute
+	  out.println(str);
+	
+		
+		
+	}
+
+}
